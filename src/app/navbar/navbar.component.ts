3c433d6ae69c33c5560e568b4cd44c945d525239
Ismaël Meziani <meziani-i@outlook.fr>
2022-03-22 12:36:35 +0100

    prob on navbar directive ngStyle

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  isLogged: boolean;
+  constructor(private route: Router, private authService: AuthService) { 
+    this.isLogged = false;
+    this.authService.changeEmitted$.subscribe((bool) => {
+      this.isLogged = bool;
+    });
+  }
 
   ngOnInit(): void {
+    const isHeLogged = this.authService.getUserId();
+      if (isHeLogged !== undefined) {
+      this.isLogged = true;
+    }
   }
+
     onClick(event:any){
     let systemDark = window.matchMedia("(prefers-color-scheme: dark)");
     systemDark.addListener(this.colorTest);
@@ -27,5 +39,15 @@ export class NavbarComponent implements OnInit {
     } else {
       document.body.setAttribute('data-theme', 'light');
     }
+  }
+
+  disconnect() {
+    this.authService.logOut().subscribe({
+      next: () => {
+        this.authService.removeUser();
+        this.isLogged = false;
+        this.route.navigate(['/home']);
+      },
+    });
   }
 }
